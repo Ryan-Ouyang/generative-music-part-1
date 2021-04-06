93c6a684a6e89e3809af3ff9adfd7626c3b2d7b9
Ryan Ouyang <ryan.ouyang01@gmail.com>
2021-04-05 21:01:03 -0400

    Express server to take inputs

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.json({ extended: true }));
+
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("This app is online.");
+});
+
+app.post("/hash", (req, res) => {
+  console.log("Got body:");
+  console.log(req.body);
+
+  res.send("Success.");
 });
 
 app.listen(port, () => {
